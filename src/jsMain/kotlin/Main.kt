41676ac7aca8fb8039df2727407466320f40f02c
Michał Borowiecki <mihbor@users.noreply.github.com>
2022-08-26 20:39:22 +0100

    move coins further apart to not overlap

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,4 +1,7 @@
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.MainScope
@@ -105,7 +108,7 @@ fun main() {
             coinObj.add(text)
             coins.add(coin to coinObj)
             scene.add(coinObj)
-            coinObj.position.x = 5 * i
+            coinObj.position.x = 8 * i
             coinObj.position.z = -40
             ball.rotation.y = -PI / 2
             text.position.set(4, 4, 0)
